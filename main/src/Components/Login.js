cc52c2d8a32f6b9fb03209a28612e99ff3cbb87c
MunaimahK <munim2k@gmail.com>
2024-01-30 00:28:37 -0500

    main page

--- main/src/Components/Login.js
+++ main/src/Components/Login.js
@@ -1,18 +1,25 @@
-import "./Login.css";
+import "./LoginStars.css";
 import { Route, Routes, Redirect } from "react-router-dom";
 function redirect() {
   console.log("Hello");
 }
 const Login = () => {
   return (
-    <body class="login">
+    <body id="star-theme">
       <div className="front-page">
-        Influx
+        <div className="container">
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+          <div class="star"></div>
+        </div>
+      </div>
+      <div className="start">
+        <h1>Influx</h1>
         <div>
           <a href="https://discord.com/api/oauth2/authorize?client_id=1179068530273034290&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Fdashboard&scope=identify+email">
-            <button onClick="redirect()" className="btn-start">
-              Start Now
-            </button>
+            Start Now
           </a>
         </div>
       </div>
